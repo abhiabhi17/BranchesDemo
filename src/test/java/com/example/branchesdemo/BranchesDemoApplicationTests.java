b52f81820dbe739c36a55a3c1c73168033f62fba
User <pavadi.abhishek@gmail.com>
2023-08-29 01:46:42 -0700

    Branches Demo initial commit

---
+++ src/test/java/com/example/branchesdemo/BranchesDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.branchesdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BranchesDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
