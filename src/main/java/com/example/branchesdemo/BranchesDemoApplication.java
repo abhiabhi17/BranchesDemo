611ef5b302c0a3ae17f990f0bf7fc6896fd48f9c
User <pavadi.abhishek@gmail.com>
2023-08-29 03:59:58 -0700

    added push_to_dev_qat_and_prod method

--- src/main/java/com/example/branchesdemo/BranchesDemoApplication.java
+++ src/main/java/com/example/branchesdemo/BranchesDemoApplication.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.sql.SQLOutput;
+
 
 @SpringBootApplication
 @RestController
@@ -30,6 +32,11 @@ public class BranchesDemoApplication {
     {
         System.out.println("this is from master");
 
+    }
+
+    public void push_to_dev_and_qat_and_prod()
+    {
+        System.out.println("This is from masster ");
     }
     public static void main(String[] args) {
         SpringApplication.run(BranchesDemoApplication.class, args);
