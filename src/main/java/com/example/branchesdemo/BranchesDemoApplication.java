c25981c9e97beeeb924742c29f73b094d9cc400e
User <pavadi.abhishek@gmail.com>
2023-08-29 03:30:37 -0700

    starting from first

--- src/main/java/com/example/branchesdemo/BranchesDemoApplication.java
+++ src/main/java/com/example/branchesdemo/BranchesDemoApplication.java
@@ -17,26 +17,10 @@ public class BranchesDemoApplication {
     {
         return "Branches demo";
     }
-    public void thisisfromqa()
-    {
-        System.out.println("statred testing code");
-    }
 
-    public  void  this_is_from_devreal()
-    {
-        System.out.println("This is dev real branch");
-    }
-    public void thisisfromqa1()
-    {
-        System.out.println("this is qa1 child branch");
-    }
     public static void main(String[] args) {
         SpringApplication.run(BranchesDemoApplication.class, args);
-        System.out.println("This is master branch");
-        System.out.println("this is from dev before merge");
-        System.out.println("This is devl brnh");
 
-        System.out.println("Hello");
     }
 
 }
