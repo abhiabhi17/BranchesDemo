61ce757d0b4e3dc7a6cad0dc2bb71c1489999ac9
User <pavadi.abhishek@gmail.com>
2023-08-29 01:53:48 -0700

    Added sop (This is master branch)

--- src/main/java/com/example/branchesdemo/BranchesDemoApplication.java
+++ src/main/java/com/example/branchesdemo/BranchesDemoApplication.java
@@ -19,6 +19,8 @@ public class BranchesDemoApplication {
     }
     public static void main(String[] args) {
         SpringApplication.run(BranchesDemoApplication.class, args);
+        System.out.println("This is master branch");
     }
+
 
 }
